13552f7e4494cb0ed38d4cade46f77826ead24bd
Moriah Taylor <moriahataylor@gmail.com>
2021-04-30 17:35:48 -0400

    update ERD

--- EmployeeSQL/QuickDBD-export.sql
+++ EmployeeSQL/QuickDBD-export.sql
@@ -6,7 +6,7 @@ CREATE TABLE "departments" (
     "dept_no" VARCHAR(255)   NOT NULL,
     "dept_name" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_name"
+        "dept_no"
      )
 );
 
@@ -14,13 +14,16 @@ CREATE TABLE "dept_emp" (
     "emp_no" INTEGER   NOT NULL,
     "dept_no" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_dept_emp" PRIMARY KEY (
-        "dept_no"
+        "emp_no","dept_no"
      )
 );
 
 CREATE TABLE "dept_manager" (
     "dept_no" VARCHAR(255)   NOT NULL,
-    "emp_no" INTEGER   NOT NULL
+    "emp_no" INTEGER   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "employees" (
@@ -40,20 +43,23 @@ CREATE TABLE "titles" (
     "title_id" VARCHAR(255)   NOT NULL,
     "title" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_titles" PRIMARY KEY (
-        "title_id","title"
+        "title_id"
      )
 );
 
 CREATE TABLE "salaries" (
     "emp_no" INTEGER   NOT NULL,
-    "salary" INTEGER   NOT NULL
+    "salary" INTEGER   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
 );
-
-ALTER TABLE "departments" ADD CONSTRAINT "fk_departments_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "dept_emp" ("dept_no");
 
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "dept_emp" ("dept_no");
