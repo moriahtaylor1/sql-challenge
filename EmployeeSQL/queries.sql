77743a96b8dc6605a7ae9617d2c4c1689a76b545
Moriah Taylor <moriahataylor@gmail.com>
2021-04-30 18:33:55 -0400

    queries 6 thru 8

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -37,9 +37,24 @@ AND last_name LIKE 'B%'
 ;
 
 -- 6. all employees in the Sales department with their employee number, last name, first name, and department name.
-
+SELECT e.emp_no, last_name, first_name, dept.dept_name
+FROM employees AS e
+JOIN dept_emp AS e_dept
+	ON e.emp_no = e_dept.emp_no
+JOIN departments AS dept
+	ON e_dept.dept_no = dept.dept_no
+WHERE dept.dept_name = 'Sales'
+;
 
 -- 7. all employees in the Sales and Development departments with their employee number, last name, first name, and department name.
-
+SELECT e.emp_no, last_name, first_name, dept.dept_name
+FROM employees AS e
+JOIN dept_emp AS e_dept
+	ON e.emp_no = e_dept.emp_no
+JOIN departments AS dept
+	ON e_dept.dept_no = dept.dept_no
+WHERE dept.dept_name = 'Sales'
+OR dept.dept_name 
+;
 
 -- 8. list the frequency count of employee last names (how many employees share each last name) in descending order
