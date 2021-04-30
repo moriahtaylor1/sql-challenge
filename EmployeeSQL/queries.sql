1be99869e156889ce8f2ff182c33a8d03aca5d8e
Moriah Taylor <moriahataylor@gmail.com>
2021-04-30 18:12:08 -0400

    query 2

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,25 +1,27 @@
--- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+-- 1. employee number, last name, first name, sex, and salary.
 SELECT E.emp_no, last_name, first_name, sex, salary
 FROM employees AS E
 JOIN salaries AS S
 	ON E.emp_no = S.emp_no;
 	
--- 2. List first name, last name, and hire date for employees who were hired in 1986.
+-- 2. first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE EXTRACT(YEAR FROM hire_date) = 1986;
+
+-- 3. manager of each department with: department number, department name, the manager's employee number, last name, first name.
 
 
--- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+-- 4. department of each employee with: employee number, last name, first name, and department name.
 
 
--- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+-- 5. first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
 
--- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+-- 6. all employees in the Sales department with their employee number, last name, first name, and department name.
 
 
--- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+-- 7. all employees in the Sales and Development departments with their employee number, last name, first name, and department name.
 
 
--- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
-
--- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+-- 8. list the frequency count of employee last names (how many employees share each last name) in descending order
